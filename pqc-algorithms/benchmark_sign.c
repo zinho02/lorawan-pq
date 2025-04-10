448891f4570391b4ea29b1b64d0defd5f3fa774a
Matheus de Oliveira Saldanha <zinhoo02@gmail.com>
2025-04-10 20:36:39 -0300

    Update benchmark_sign.c

--- pqc-algorithms/benchmark_sign.c
+++ pqc-algorithms/benchmark_sign.c
@@ -9,7 +9,7 @@
 #define ITERATIONS 10000
 #define MESSAGE_LEN 32
 
-int interrupt_mode = 0; // Global flag to control interrupt behavior
+int interrupt_mode = 0; // Global flag for interrupt mode
 
 // Timing helper
 double time_diff_ns(struct timespec start, struct timespec end) {
@@ -35,51 +35,73 @@ void wait_for_continue(const char *op_name) {
     }
 }
 
-stats_t benchmark_operation(void (*operation)(void), int iterations, const char *op_name) {
-    double *times = malloc(iterations * sizeof(double));
-    struct timespec start, end;
+stats_t benchmark_combined(int iterations) {
+    double *keygen_times = malloc(iterations * sizeof(double));
+    double *sign_times = malloc(iterations * sizeof(double));
+    double *verify_times = malloc(iterations * sizeof(double));
+
+    unsigned char msg[MESSAGE_LEN];
+    memset(msg, 0xA5, sizeof(msg)); // Fixed message
 
     for (int i = 0; i < iterations; i++) {
-        wait_for_continue(op_name);
+        unsigned char pk[CRYPTO_PUBLICKEYBYTES];
+        unsigned char sk[CRYPTO_SECRETKEYBYTES];
+        unsigned char signed_msg[MESSAGE_LEN + CRYPTO_BYTES];
+        unsigned long long signed_len;
+        unsigned char unsigned_msg[MESSAGE_LEN + CRYPTO_BYTES];
+        unsigned long long unsigned_len;
+
+        struct timespec start, end;
+
+        // Keygen
+        wait_for_continue("Key Generation");
         clock_gettime(CLOCK_MONOTONIC, &start);
-        operation();
+        crypto_sign_keypair(pk, sk);
         clock_gettime(CLOCK_MONOTONIC, &end);
-        times[i] = time_diff_ns(start, end);
+        keygen_times[i] = time_diff_ns(start, end);
+
+        // Sign
+        wait_for_continue("Signing");
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        crypto_sign(signed_msg, &signed_len, msg, MESSAGE_LEN, sk);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        sign_times[i] = time_diff_ns(start, end);
+
+        // Verify
+        wait_for_continue("Verification");
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        crypto_sign_open(unsigned_msg, &unsigned_len, signed_msg, signed_len, pk);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        verify_times[i] = time_diff_ns(start, end);
     }
 
-    double sum = 0;
-    for (int i = 0; i < iterations; i++) sum += times[i];
-    double mean = sum / iterations;
+    // Helper lambda for average and stddev
+    auto compute_stats = [](double *times, int count) -> stats_t {
+        double sum = 0;
+        for (int i = 0; i < count; i++) sum += times[i];
+        double mean = sum / count;
 
-    double variance = 0;
-    for (int i = 0; i < iterations; i++)
-        variance += pow(times[i] - mean, 2);
-    variance /= iterations;
-    double stddev = sqrt(variance);
+        double variance = 0;
+        for (int i = 0; i < count; i++) variance += pow(times[i] - mean, 2);
+        variance /= count;
 
-    free(times);
-    return (stats_t){ mean / 1e6, stddev / 1e6 }; // Return in ms
-}
+        return (stats_t){ mean / 1e6, sqrt(variance) / 1e6 }; // in ms
+    };
 
-// Wrappers for NIST API functions
-unsigned char pk[CRYPTO_PUBLICKEYBYTES];
-unsigned char sk[CRYPTO_SECRETKEYBYTES];
-unsigned char msg[MESSAGE_LEN];
-unsigned char signed_msg[MESSAGE_LEN + CRYPTO_BYTES];
-unsigned long long signed_len;
-unsigned char unsigned_msg[MESSAGE_LEN + CRYPTO_BYTES];
-unsigned long long unsigned_len;
+    stats_t keygen_stats = compute_stats(keygen_times, iterations);
+    stats_t sign_stats = compute_stats(sign_times, iterations);
+    stats_t verify_stats = compute_stats(verify_times, iterations);
 
-void op_keygen(void) {
-    crypto_sign_keypair(pk, sk);
-}
+    free(keygen_times);
+    free(sign_times);
+    free(verify_times);
 
-void op_sign(void) {
-    crypto_sign(signed_msg, &signed_len, msg, MESSAGE_LEN, sk);
-}
+    printf("\nResults over %d iterations:\n", iterations);
+    printf("Key Generation:   Avg = %.3f ms, StdDev = %.3f ms\n", keygen_stats.avg, keygen_stats.stddev);
+    printf("Signing:          Avg = %.3f ms, StdDev = %.3f ms\n", sign_stats.avg, sign_stats.stddev);
+    printf("Verification:     Avg = %.3f ms, StdDev = %.3f ms\n", verify_stats.avg, verify_stats.stddev);
 
-void op_verify(void) {
-    crypto_sign_open(unsigned_msg, &unsigned_len, signed_msg, signed_len, pk);
+    return keygen_stats; // Just to satisfy return type, not used
 }
 
 int main(int argc, char *argv[]) {
@@ -89,20 +111,7 @@ int main(int argc, char *argv[]) {
     }
 
     printf("Benchmarking NIST PQC Signature Scheme: %s\n", CRYPTO_ALGNAME);
-    memset(msg, 0xA5, sizeof(msg)); // Sample fixed message
-
-    crypto_sign_keypair(pk, sk);
-    crypto_sign(signed_msg, &signed_len, msg, MESSAGE_LEN, sk);
-    crypto_sign_open(unsigned_msg, &unsigned_len, signed_msg, signed_len, pk);
-
-    stats_t keygen_stats = benchmark_operation(op_keygen, ITERATIONS, "Key Generation");
-    stats_t sign_stats = benchmark_operation(op_sign, ITERATIONS, "Signing");
-    stats_t verify_stats = benchmark_operation(op_verify, ITERATIONS, "Verification");
-
-    printf("\nResults over %d iterations:\n", ITERATIONS);
-    printf("Key Generation:   Avg = %.3f ms, StdDev = %.3f ms\n", keygen_stats.avg, keygen_stats.stddev);
-    printf("Signing:          Avg = %.3f ms, StdDev = %.3f ms\n", sign_stats.avg, sign_stats.stddev);
-    printf("Verification:     Avg = %.3f ms, StdDev = %.3f ms\n", verify_stats.avg, verify_stats.stddev);
-
+    benchmark_combined(ITERATIONS);
     return 0;
 }
+
